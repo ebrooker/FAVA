bf7a984b567054456acada12b06f7a526a1d507e
Ezra Brooker <ezrabrooker@Ezras-MacBook-Pro.local>
2023-12-03 22:27:03 -0500

    [UTIL] add timer decorator for timing functions

---
+++ fava/util/__init__.py
@@ -0,0 +1,13 @@
+
+
+import time
+
+
+def timer(func):
+    def decorator(*args, **kwargs):
+        tbeg = time.perf_counter()
+        result = func(*args, **kwargs)
+        tend = time.perf_counter()
+        print(f"{func.__name__} --> {tend-tbeg:2.4f}")
+        return result
+    return decorator
